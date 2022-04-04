762a9582566a752974345e4b59ec3022bc7ddd89
SoumenRaj398 <soumenraj66@gmail.com>
2022-04-05 00:08:33 +0600

    not found compo

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Route path ="/" element = {<Home></Home>}></Route>
         <Route path ="/home" element = {<Home></Home>}></Route>
         <Route path ="review" element = {<Review></Review>}></Route>
-        
+        <Route path ="*" element = {<Notfound></Notfound>}></Route>
       </Routes>
       
     </div>
